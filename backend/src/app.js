1cdf9c4c819b45f83ac35d957c7d6aeaaddf3d0f
pabhi0901 <pabhishek7333@gmail.com>
2026-03-02 11:19:12 +0530

    added frontend url in app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import cors from "cors"
 
 //middlewares
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'],
+  origin: ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002',process.env.frontendURL],
   credentials: true
 }))
 app.use(express.json())
